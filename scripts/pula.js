fe9d9aca5809b8a4859ab761afd0bfbde542aad5
Alex Stasiewicz <adlerzz.true@gmail.com>
2024-01-23 15:52:12 +0300

    Create pula.js

---
+++ scripts/pula.js
@@ -0,0 +1,5 @@
+document.querySelector("div#default").remove();
+const customDiv = document.createElement("div");
+customDiv.setAttribute("id", "customDiv");
+customDiv.innerText = "Dynamically added HTML element";
+document.body.append();
