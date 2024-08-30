dadea387ec2ad3354e9c083bbcd7420843126c83
alst0816 <alexandr.stasewich@netcracker.com>
2024-08-30 20:25:57 +0300

    error handling

--- apps/svetilo-ctrl/script.js
+++ apps/svetilo-ctrl/script.js
@@ -11,33 +11,45 @@ const terminalEl = document.getElementById("terminal");
 
 
 connectEl.addEventListener("click", async () => {
-	port = await navigator.serial.requestPort();
+    port = connect();
+   
+    if(!port){
+        return;
+    }
 
-	await port.open({ baudRate: 9600 });
-	[reader, writer] = [
-		port.readable.getReader(),
-		port.writable.getWriter(),
-	];
-	setTimeout(async () => {
-		do {
-			const { value, done } = await reader.read();
-			if (done) {
-				reader.releaseLock();
-				break;
-			}
-			const response = decoder.decode(value)
-			console.log(response);
-			terminalEl.value = terminalEl.value + "\n" + `<< ${response}`;
-		} while (true);
-	}, 0);
+    await port.open({ baudRate: 9600 });
+    [reader, writer] = [
+        port.readable.getReader(),
+        port.writable.getWriter(),
+    ];
+    setTimeout(async () => {
+        do {
+            const { value, done } = await reader.read();
+            if (done) {
+                reader.releaseLock();
+                break;
+            }
+            const response = decoder.decode(value)
+            console.log(response);
+            terminalEl.value = terminalEl.value + "\n" + `<< ${response}`;
+        } while (true);
+    }, 0);
 
 });
 
+async function connect(){
+    try{
+        return await navigator.serial.requestPort();
+    } catch(e) {
+        terminalEl.value = terminalEl.value + "\n" + `!! ${e}`;
+    } 
+}
+
 commandEl.addEventListener("keyup", async (event) => {
-	if (event.key === "Enter") {
-		const commandText = commandEl.value;
-		commandEl.value = "";
-		terminalEl.value = terminalEl.value + "\n" + `>> ${commandText}`; 
-		await writer.write(encoder.encode(commandText));
-	}
+    if (event.key === "Enter") {
+        const commandText = commandEl.value;
+        commandEl.value = "";
+        terminalEl.value = terminalEl.value + "\n" + `>> ${commandText}`; 
+        await writer.write(encoder.encode(commandText));
+    }
 });
